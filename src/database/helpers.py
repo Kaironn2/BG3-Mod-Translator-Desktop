a46fca6e4dd5c29b38b5129730733704b424b129
JonhDataOpp <jonathas_oliveira17@hotmail.com>
2026-05-04 00:03:32 -0300

    chore: start electron + typescript version

--- src/database/helpers.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import Session
-from typing import Any, List
-
-class DatabaseHelper:
-    
-    @staticmethod
-    def add_and_commit(session: Session, obj: Any) -> None:
-        session.add(obj)
-        session.commit()
